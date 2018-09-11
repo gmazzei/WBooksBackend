735f451d5d2bfba6df8ec0f2d55361420e2291db
Gabriel Mazzei <gmazzei91@gmail.com>
2018-09-11 14:04:09 -0300

    Linux compatile

--- Sources/App/Utils/TokenGenerator.swift
+++ Sources/App/Utils/TokenGenerator.swift
@@ -1,4 +1,9 @@
+#if os(Linux)
+import Glibc
+import SwiftShims
+#else
 import Darwin
+#endif
 
 class TokenGenerator {
     
@@ -8,11 +13,19 @@ class TokenGenerator {
         let allowedCharsCount = UInt32(allowedChars.count)
         var randomString = ""
         for _ in 0..<length {
-            let randomNumber = Int(arc4random_uniform(allowedCharsCount))
+            let randomNumber = Int(cs_arc4random_uniform(upperBound: allowedCharsCount))
             let randomIndex = allowedChars.index(allowedChars.startIndex, offsetBy: randomNumber)
             let newCharacter = allowedChars[randomIndex]
             randomString += String(newCharacter)
         }
         return randomString
+    }
+    
+    private class func cs_arc4random_uniform(upperBound: UInt32) -> UInt32 {
+        #if os(Linux)
+        return _swift_stdlib_arc4random_uniform(upperBound)
+        #else
+        return arc4random_uniform(upperBound)
+        #endif
     }
 }
