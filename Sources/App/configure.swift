693a1b4d73dccfe0aeaf345a03417ee60fbdee25
Gabriel Mazzei <gmazzei91@gmail.com>
2018-09-13 11:02:38 -0300

    PostgreSQL config changed

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -16,6 +16,7 @@ public func configure(
 
     try services.register(FluentPostgreSQLProvider())
     
+    /*
     let postgresqlConfig = PostgreSQLDatabaseConfig(
         hostname: "127.0.0.1",
         port: 5432,
@@ -23,7 +24,7 @@ public func configure(
         database: "wbooks",
         password: nil
     )
- 
+    */
     /*
     let postgresqlConfig = PostgreSQLDatabaseConfig(
         hostname: "ec2-75-101-153-56.compute-1.amazonaws.com",
@@ -36,7 +37,9 @@ public func configure(
     /*
     let postgresqlConfig = PostgreSQLDatabaseConfig(url: "postgres://shkiiqdbrpuwci:28bf3122ca40d885c6bcfaee5b8f6a6af58f59b8ba1d3515d06f68b4afb2aff2@ec2-75-101-153-56.compute-1.amazonaws.com:5432/d7p1hi5krji6i8").unsafelyUnwrapped
     */
-    services.register(postgresqlConfig)
+    
+    //let postgresqlConfig = PostgreSQLDatabaseConfig.default()
+    //services.register(postgresqlConfig)
     
     var migrations = MigrationConfig()
     migrations.add(model: User.self, database: .psql)
