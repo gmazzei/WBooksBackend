e60984f235050e1e50fb38f28c2c0ea982d5a580
Gabriel Mazzei <gmazzei91@gmail.com>
2018-09-13 15:08:27 -0300

    Refactor InvalidCredentialsError

--- Sources/App/Controllers/AuthenticationController.swift
+++ Sources/App/Controllers/AuthenticationController.swift
@@ -4,26 +4,6 @@ import FluentPostgreSQL
 
 final class AuthenticationController {
     
-    /*
-    func createAuthenticationToken(_ request: Request) throws -> Future<User.PublicUser> {
-        return try request.content.decode(User.self)
-            .flatMap(to: User.PublicUser.self) { user in
-            
-            let passwordHashed = try request.make(BCryptDigest.self).hash(user.password)
-            let newUser = User(username: user.username, password: passwordHashed)
-            
-            return newUser.save(on: request).flatMap(to: User.PublicUser.self) { createdUser in
-                let accessToken = try Token.createToken(forUser: createdUser)
-                return accessToken.save(on: request).map(to: User.PublicUser.self) { createdToken in
-                    let publicUser = User.PublicUser(username: createdUser.username, token: createdToken.token)
-                    return publicUser
-                }
-            }
-            
-        }
-    }
-    */
-    
     func createAuthenticationToken(_ request: Request) throws -> Future<User.PublicUser> {
         var plainTextPassword: String? = .none
         
@@ -34,7 +14,7 @@ final class AuthenticationController {
                 return User.query(on: request)
                     .filter(\User.username == user.username)
                     .first()
-                    .unwrap(or: Abort(.notFound)) // TODO: Change for appropiate error
+                    .unwrap(or: InvalidCredentialsError())
     
             }.flatMap(to: User.PublicUser.self) { user in
                 let passwordMatches = try BCrypt.verify(plainTextPassword!, created: user.password)
@@ -46,14 +26,15 @@ final class AuthenticationController {
                         return publicUser
                     }
                 } else {
-                    throw WrongPasswordError()
+                    throw InvalidCredentialsError()
                 }
         }
     
     }
 }
 
-class WrongPasswordError: AbortError {
+
+private class InvalidCredentialsError: AbortError {
     var status: HTTPResponseStatus
     var reason: String
     var identifier: String
